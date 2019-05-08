ec5bc532696b89d765c787dce8891b9daf83ddc9
DeisherJohn <john.deisher2013@gmail.com>
2019-05-08 16:06:03 +1000

    added template for p15

---
+++ PythonSolutions/p15.py
@@ -0,0 +1,14 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   Program: Uniform Random Elem Picker
+#   Daily Problem #: 15
+#   Author: John Deisher
+#   Date Started: 5/8/2019
+#   Date Finished: 5/8/2019
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+This problem was asked by Facebook.
+
+Given a stream of elements too large to store in memory, 
+pick a random element from the stream with uniform probability.
+"""

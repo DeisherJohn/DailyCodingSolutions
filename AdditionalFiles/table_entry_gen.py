3cea0985ab436ed252c9063a6af253bd9310b3f7
DeisherJohn <john.deisher2013@gmail.com>
2019-05-11 22:30:24 +1000

    fixed messed up link for main table

--- AdditionalFiles/table_entry_gen.py
+++ AdditionalFiles/table_entry_gen.py
@@ -5,7 +5,7 @@
 index = 1
 Summery = "Find a given sum in a list of int"
 gitPyPath = "https://github.com/DeisherJohn/DailyCodingProblems/blob/master/PythonSolutions/p"
-Details = "[Link](2019-05-11-problem1-solution"
+Details = "2019-05-11-problem1-solution"
 pathDetail = "[Link](https://deisherjohn.github.io/DailyCodingProblems/"
 WIP = ""
 

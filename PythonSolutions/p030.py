23509d5ba373051a01a73710ae2308181cfc3f4f
DeisherJohn <john.deisher2013@gmail.com>
2019-05-14 00:58:15 +1000

    finished p30 python solution

--- PythonSolutions/p030.py
+++ PythonSolutions/p030.py
@@ -20,3 +20,67 @@ For example, given the input [2, 1, 2], we can hold 1 unit of water in the middl
 Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, 
 and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 """
+
+def compute_held_volume(topograph):
+
+    added_set = set()
+    current_max = topograph[0]
+    current_max_index = 0
+    current_sum = 0
+    in_valley = False
+    returnSum = 0
+
+    for i, elevation in enumerate(topograph):
+        if (elevation >= current_max) and in_valley is False:
+            current_max = elevation
+            current_max_index = i 
+
+        if elevation < current_max:
+            in_valley = True
+            current_sum += elevation
+
+        if elevation >= current_max and in_valley is True:
+            if (min(current_max_index, i),max(current_max_index, i)) not in added_set:
+                area = current_max * ((i-1)-current_max_index)
+                returnSum += area - current_sum
+                added_set.add((min(current_max_index, i),max(current_max_index, i)))
+            current_max = elevation
+            current_sum = 0
+            current_max_index = i
+            in_valley = False
+
+    current_max = topograph[-1]
+    current_max_index = len(topograph)
+    current_sum = 0
+    in_valley = False
+
+    for i in range(len(topograph)-1,0,-1):
+        i_elevation = topograph[i]
+        if i_elevation >= current_max and in_valley is False:
+            current_max = i_elevation
+            current_max_index = i 
+
+        if i_elevation < current_max:
+            in_valley = True
+            current_sum += i_elevation
+
+        if i_elevation >= current_max and in_valley is True:
+            if (min(current_max_index, i),max(current_max_index, i)) not in added_set:
+                area = current_max * (current_max_index-1 - i)
+                returnSum += area - current_sum
+                added_set.add((min(current_max_index, i),max(current_max_index, i)))
+            current_max = i_elevation
+            current_sum = 0
+            current_max_index = i
+            in_valley = False
+
+    print(returnSum)
+
+
+    pass
+
+# test = [3,6,3,3,0,3,6,5,4,5,4,6]
+# test = [2, 1, 2]
+test = [3, 0, 1, 3, 0, 5]
+
+compute_held_volume(test)

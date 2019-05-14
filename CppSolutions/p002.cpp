be4fb024adb4cbea9289bb423acb319630e0dbcf
DeisherJohn <john.deisher2013@gmail.com>
2019-05-14 14:59:52 +1000

    finished cpp for p2 and 3

--- CppSolutions/p002.cpp
+++ CppSolutions/p002.cpp
@@ -21,3 +21,48 @@ Use a doubly linked list of nodes that carry the product of the nodes before and
 once it is built it only requires one pass to get the list back of all nodes with no division
 """
 */
+
+#include <vector>
+#include <iostream> 
+
+using namespace std;
+
+vector<int> get_product(vector<int> v){
+    vector<int> return_vector;
+    int right_product = 1;
+    return_vector.push_back(1);
+
+    for(int i = 0 ; i < v.size() - 1 ; i++){
+        //int new_product = left_product[i-1] * v[i-1];
+        return_vector.push_back(return_vector[i] * v[i]);
+    }
+
+    for(int i = v.size() - 1 ; i >= 0 ; i--){
+        right_product *= v[i];
+        return_vector[i-1] *= right_product;
+    }
+    return return_vector;
+
+}
+
+int main(){
+    vector<int> v;
+
+    for (int i = 1; i < 6; ++i)
+    {
+        /* code */
+        v.push_back(i);
+    }
+
+    vector<int> new_v = get_product(v);
+
+    for (int i = 0; i < new_v.size(); ++i)
+    {
+        /* code */
+        cout << new_v[i] << ", ";
+    }
+    cout << endl;
+
+    
+    return 0;
+}
--- CppSolutions/p003.cpp
+++ CppSolutions/p003.cpp
@@ -9,7 +9,9 @@
 """
 This problem was asked by Google.
 
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+Given the root to a binary tree, implement serialize(root), 
+which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree.
 
 For example, given the following Node class
 
@@ -31,3 +33,109 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
         /       \
     left.left   null
 """*/
+
+#include <string>
+#include <iostream> 
+#include <list>
+
+using namespace std;
+
+class node{
+public:
+    string value;
+    node *left;
+    node *right;
+
+    node(){
+        value = "NULL";
+        left = NULL;
+        right = NULL;
+
+    }
+    ~node();
+
+    void Build_Node(string val, node * left_node, node * right_node){
+        value = val;
+        left = left_node;
+        right = right_node;
+    }
+};
+
+string serialize(node * root){
+    string myString = "";
+    string leftString = "";
+    string rightString = "";
+
+    if (root->left != NULL){ 
+        leftString = serialize(root->left);
+    } else {
+        leftString = "NULL";
+    }
+    if (root->right != NULL){ 
+        rightString = serialize(root->right);
+    } else {
+        rightString = "NULL";
+    }
+
+    myString = root->value + ";" + leftString + ";" + rightString;
+    return myString;
+}
+
+node * deserialize(string * s){
+    if (s->size() == 0){
+        return NULL;
+    }
+
+    string myString = "";
+    node * myNode = new node();
+    while(s->size() > 0){
+        if(s->at(0) == ';'){
+            //end of name
+            if(myString != "NULL"){
+                myNode->value = myString;
+                myString = "";
+            } else {
+                return NULL;
+            }
+            s = &s->replace(0,1,"");
+            myNode->left = deserialize(s);
+            s = &s->replace(0,1,"");
+            myNode->right = deserialize(s);
+            return myNode;
+        } else {
+            myString += s->at(0);
+            s = &s->replace(0,1,"");
+        }
+        
+    }
+    return myNode;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    node * root = new node();
+    node * left = new node();
+    node * right = new node();
+    node * leftleft = new node();
+
+    
+    leftleft->Build_Node("leftleft", NULL, NULL);
+    left->Build_Node("left", leftleft, NULL);
+    right->Build_Node("right", NULL, NULL);
+    root->Build_Node("root", left, right);
+
+    //cout << root->right->value << endl;
+
+    serialize(root);
+    string data = serialize(root);
+    string * testSent = &data;
+    node * newRoot = deserialize(testSent);
+
+    if (newRoot->left->left->value == root->left->left->value){
+        cout << "Tree structures match" << endl;
+    } else {
+        cout << "Trees did not match" << endl;
+    }
+    return 0;
+}
